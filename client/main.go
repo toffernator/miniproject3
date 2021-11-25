17a6dcbeeeb97f987c98c1d3039deb33dfbb8593
Christoffer Gram <chkg@itu.dk>
2021-11-25 15:55:52 +0100

    First (Untested) Client Impl

---
+++ client/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/manifoldco/promptui"
+	"github.com/toffernator/miniproject3/api"
+	"google.golang.org/grpc"
+)
+
+var (
+	nameFlag    = flag.String("name", "name", "The name of your user")
+	addressFlag = flag.String("address", "localhost:50000", "The address of the front-end to connect to")
+)
+
+func main() {
+	conn, err := grpc.Dial(*addressFlag, grpc.WithInsecure, grpc.WithBlock)
+	must(err)
+	client := api.NewAuctionClient(conn)
+
+	clientLoop(client)
+}
+
+func clientLoop(c api.AuctionClient) {
+	isDone := false
+	for !isDone {
+		prompt := promptui.Select{
+			Label: "Choose an option",
+			Items: []string{"Bid", "Result", "Quit"},
+		}
+
+		_, result, err := prompt.Run()
+		must(err)
+
+		switch result {
+		case "Bid":
+			Bid(c)
+		case "Result":
+			Result(c)
+		case "Quit":
+			Leave()
+		}
+	}
+}
+
+func Bid(c api.AuctionClient) {
+	bidPrompt := promptui.Prompt{
+		Label: "Amount: ",
+	}
+
+	amount, err := bidPrompt.Run()
+	must(err)
+
+	intAmount, err := strconv.Atoi(amount)
+	must(err)
+
+	payload := api.BidMsg{
+		Amount: int32(intAmount),
+		User:   *nameFlag,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	c.Bid(ctx, &payload)
+}
+
+func Result(c api.AuctionClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	payload := api.Empty{}
+	outcome, _ := c.Result(ctx, &payload)
+
+	log.Printf("Result: %d by %s", outcome.ResultOrHighest, outcome.Winner)
+}
+
+func Leave() {
+	os.Exit(0)
+}
+
+func must(err error) {
+	if err != nil {
+		log.Fatalf("Failed with err code: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/toffernator/miniproject3
 go 1.17
 
 require (
+	github.com/manifoldco/promptui v0.9.0
 	google.golang.org/grpc v1.42.0
 	google.golang.org/protobuf v1.27.1
 )
 
 require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
