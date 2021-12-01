8bd5569ba4812bd9c6815beafac622199b4fb997
Christoffer Gram <chkg@itu.dk>
2021-12-01 13:05:52 +0100

    User Registration + Some Error Handling

--- RM/main.go
+++ RM/main.go
@@ -22,6 +22,7 @@ func main() {
 }
 
 type RMServer struct {
+	isRegistered map[string]bool
 	api.UnimplementedRMServer
 }
 
@@ -30,6 +31,11 @@ func (as *RMServer) Bid(ctx context.Context, bm *api.BidMsg) (*api.Ack, error) {
 	defer lock.Unlock()
 
 	log.Printf("Received Bid")
+
+	if !as.isRegistered[bm.User] {
+		as.isRegistered[bm.User] = true
+		log.Printf("Registered %s to the auction", bm.User)
+	}
 
 	if bm.Amount > highestBid {
 		log.Printf("Bid was accepted with a value: %d | Previous highest bid: %d", bm.Amount, highestBid)
@@ -73,7 +79,9 @@ func startRMServer() {
 	}
 
 	grpcServer := grpc.NewServer()
-	server := RMServer{}
+	server := RMServer{
+		isRegistered: make(map[string]bool, 0),
+	}
 
 	api.RegisterRMServer(grpcServer, &server)
 	log.Printf("RM Server listening to %s\n", lis.Addr())
